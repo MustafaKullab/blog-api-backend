44eb8f84f2ff5211eeace60ea36e866ee43c7f7f
Mustafa Kullab <momoana013@gmail.com>
2026-06-22 12:07:57 +0300

    fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
 // allow the front end to access to the back end server
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true,
   })
 );
